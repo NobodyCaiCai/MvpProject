a34b855e6bcb94b2ef26e8e33bcce93bb1919398
caicai <handashuang@bilibili.com>
2024-07-25 12:06:44 +0800

    mvp优化：BaseFragment 通用式封装， 静态代理模式的运用

--- app/src/main/java/com/caicai/mvpstudyproject/mvp/v2/basemvp/IBaseActivity.kt
+++ app/src/main/java/com/caicai/mvpstudyproject/mvp/v2/basemvp/IBaseActivity.kt
@@ -2,6 +2,9 @@ package com.caicai.mvpstudyproject.mvp.v2.basemvp
 
 import android.content.Context
 
+/**
+ * https://blog.csdn.net/smile_Running/article/details/94724540
+ */
 interface IBaseActivity {
     fun getContext(): Context?
 }
